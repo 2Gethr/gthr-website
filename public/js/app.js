19ac173818b7f2b64b47557fda5e6b6c798084de
Brice Thomas <brice.p.thomas@gmail.com>
2015-02-23 21:25:21 +0100

    Testing stuff & cie.

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,5 @@
-console.log('Hey!');
+console.log('Well done!');
+console.log('You\'re pretty awesome.');
+console.log('JS files are correctly served :)\n\n\n');
+console.log('I want a Mojito.');
+console.log('Or more...');
